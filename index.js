e74545ee4d0e0e2eb4e211abb7e6b8afd0d73153
Kizze Harris <kharris668@insite.4cd.edu>
2017-09-14 02:26:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
     var lowerCase = string.toLowerCase();
     return ("I can't hear you!");
   }else if (string === string.toUpperCase()) {
-    var upperCase = "YES INDEED!";
+    var upperCase = 'YES INDEED!';
   }else {
     var mixedCase = "I love you, too.";
     return(mixedCase);
