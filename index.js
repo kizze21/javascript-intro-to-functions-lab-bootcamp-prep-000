7c459c1e264909d562477af915a729410d82ed03
Kizze Harris <kharris668@insite.4cd.edu>
2017-09-14 02:24:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ function whisper(string){
 }
 
 function logShout(string){
-  console.log("string.toUpperCase()");
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()){
+    var lowerCase = string.toLowerCase();
+    return ("I can't hear you!");
+  }else if (string === string.toUpperCase()) {
+    var upperCase = "YES INDEED!";
+  }else {
+    var mixedCase = "I love you, too.";
+    return(mixedCase);
+  }
+}
+
